dc83768868b2533c44764c6b17ea20a3c0fec28e
int2e <1013706666@qq.com>
2019-12-04 13:23:59 +0800

    优化短路表达式

--- src/HPSocket.Net/Base/Agent.cs
+++ src/HPSocket.Net/Base/Agent.cs
@@ -766,7 +766,7 @@ namespace HPSocket.Base
                 Marshal.Copy(data, bytes, 0, length);
             }
 
-            if (ProxyList?.Count > 0 && _connProxy.ContainsKey(connId) && NativeGetConnectionExtra(connId, out var extra) && extra != IntPtr.Zero)
+            if (_connProxy.ContainsKey(connId) && ProxyList?.Count > 0 && NativeGetConnectionExtra(connId, out var extra) && extra != IntPtr.Zero)
             {
                 var nativeExtra = extra.ToNativeExtra();
                 if (nativeExtra.ProxyConnectionState == ProxyConnectionState.Normal)
