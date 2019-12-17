8897c2323755fd2c6f4e4fc7acbbdb5a84cdd175
int2e <1013706666@qq.com>
2019-12-17 12:32:18 +0800

    change AssemblyInfo to project name

--- demo/SslEchoServer/Properties/AssemblyInfo.cs
+++ demo/SslEchoServer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("TcpEchoServer")]
+[assembly: AssemblyTitle("SslEchoServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TcpEchoServer")]
+[assembly: AssemblyProduct("SslEchoServer")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- demo/TcpEchoAgent/Properties/AssemblyInfo.cs
+++ demo/TcpEchoAgent/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("TcpEchoClient")]
+[assembly: AssemblyTitle("TcpEchoAgent")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TcpEchoClient")]
+[assembly: AssemblyProduct("TcpEchoAgent")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
