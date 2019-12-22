9bae2bba981d62d7fe636bfee025c5dd87339aef
int2e <1013706666@qq.com>
2019-12-22 14:26:23 +0800

    add vs2017 demos

---
+++ demo_vs2107/TcpPackAgent-TestEcho/FormAgent.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace TcpPackAgentTestEcho
+{
+    partial class FormAgent
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnGetServerTime = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnConnectSwitch = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnTest);
+            this.panel1.Controls.Add(this.btnGetServerTime);
+            this.panel1.Controls.Add(this.btnSend);
+            this.panel1.Controls.Add(this.txtContent);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.txtLog);
+            this.panel1.Controls.Add(this.btnConnectSwitch);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1214, 789);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTest.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnTest.Location = new System.Drawing.Point(809, 81);
+            this.btnTest.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(162, 54);
+            this.btnTest.TabIndex = 11;
+            this.btnTest.Text = "粘包测试";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.BtnTest_Click);
+            // 
+            // btnGetServerTime
+            // 
+            this.btnGetServerTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGetServerTime.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnGetServerTime.Location = new System.Drawing.Point(292, 81);
+            this.btnGetServerTime.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGetServerTime.Name = "btnGetServerTime";
+            this.btnGetServerTime.Size = new System.Drawing.Size(496, 54);
+            this.btnGetServerTime.TabIndex = 10;
+            this.btnGetServerTime.Text = "获取服务器时间, 模拟服务器耗时操作";
+            this.btnGetServerTime.UseVisualStyleBackColor = true;
+            this.btnGetServerTime.Click += new System.EventHandler(this.BtnGetServerTime_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSend.Location = new System.Drawing.Point(809, 13);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(162, 54);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // txtContent
+            // 
+            this.txtContent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtContent.Location = new System.Drawing.Point(292, 26);
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(496, 35);
+            this.txtContent.TabIndex = 8;
+            this.txtContent.Text = "你好，格尔曼·斯帕罗！";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClear.Location = new System.Drawing.Point(1012, 94);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(162, 54);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "清空";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.BackColor = System.Drawing.SystemColors.Window;
+            this.txtLog.Location = new System.Drawing.Point(34, 155);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(1140, 600);
+            this.txtLog.TabIndex = 6;
+            // 
+            // btnConnectSwitch
+            // 
+            this.btnConnectSwitch.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnConnectSwitch.Location = new System.Drawing.Point(34, 39);
+            this.btnConnectSwitch.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConnectSwitch.Name = "btnConnectSwitch";
+            this.btnConnectSwitch.Size = new System.Drawing.Size(220, 70);
+            this.btnConnectSwitch.TabIndex = 5;
+            this.btnConnectSwitch.Text = "连接";
+            this.btnConnectSwitch.UseVisualStyleBackColor = true;
+            this.btnConnectSwitch.Click += new System.EventHandler(this.BtnConnectSwitch_Click);
+            // 
+            // FormAgent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1214, 789);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormAgent";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TcpPackAgentTestEcho";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormAgent_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnConnectSwitch;
+        private System.Windows.Forms.Button btnGetServerTime;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button btnTest;
+    }
+}
+
